c393d7e31ec2f79b62799d09c8d0533acc705431
Fazal Khan <fazal.khan@digital.justice.gov.uk>
2015-08-05 14:38:49 +0100

    chopped up assets, made page work

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,53 @@
-var gulp = require('gulp'),
-  gutil = require('gulp-util'),
-  sass = require('gulp-sass'),
-  jade = require('gulp-jade'),
-  concat = require('gulp-concat'),
-  livereload = require('gulp-livereload'), // Livereload plugin needed: https://chrome.google.com/webstore/detail/livereload/jnihajbhpnppcggbcgedagnkighmdlei
-  tinylr = require('tiny-lr'),
-  express = require('express'),
-  app = express(),
-  path = require('path'),
-  server = tinylr();
-
+'use strict';
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var sass = require('gulp-sass');
+var jade = require('gulp-jade');
+var concat = require('gulp-concat');
+var livereload = require('gulp-livereload');
+var tinylr = require('tiny-lr');
+var express = require('express');
+var app = express();
+var path = require('path');
+var server = tinylr();
 
 // --- Basic Tasks ---
-gulp.task('css', function() {
+gulp.task('css', function () {
   return gulp.src('src/css/*.scss')
     .pipe(
-      sass( {
+      sass({
         includePaths: ['src/css'],
         errLogToConsole: true
-      } ) )
-    .pipe( gulp.dest('public/css/') )
-    .pipe( livereload( server ));
+      }))
+    .pipe(gulp.dest('public/css/'))
+    .pipe(livereload(server));
 });
 
-gulp.task('js', function() {
+gulp.task('js', function () {
   return gulp.src('src/js/*.js')
-    .pipe( concat('all.min.js'))
-    .pipe( gulp.dest('public/js/'))
-    .pipe( livereload( server ));
+    .pipe(concat('all.min.js'))
+    .pipe(gulp.dest('public/js/'))
+    .pipe(livereload(server));
 });
 
-gulp.task('templates', function() {
+gulp.task('templates', function () {
   return gulp.src('src/*.jade')
     .pipe(jade({
       pretty: true
     }))
     .pipe(gulp.dest('public/'))
-    .pipe( livereload( server ));
+    .pipe(livereload(server));
 });
 
-gulp.task('express', function() {
+gulp.task('express', function () {
   app.use(express.static(path.resolve('./public')));
   app.listen(1337);
   gutil.log('Listening on port: 1337');
+});
+
+gulp.task('static', function () {
+  return gulp.src('src/web-determinations/staticresource/**/*')
+    .pipe(gulp.dest('public/web-determinations/staticresource/'));
 });
 
 gulp.task('watch', function () {
@@ -51,14 +56,13 @@ gulp.task('watch', function () {
       return console.log(err);
     }
 
-    gulp.watch('src/css/*.scss',['css']);
+    gulp.watch('src/css/*.scss', ['css']);
 
-    gulp.watch('src/js/*.js',['js']);
+    gulp.watch('src/js/*.js', ['js']);
 
-    gulp.watch('src/*.jade',['templates']);
-
+    gulp.watch('src/*.jade', ['templates']);
   });
 });
 
 // Default Task
-gulp.task('default', ['js','css','templates','express','watch']);
+gulp.task('default', ['js', 'css', 'templates', 'static', 'express', 'watch']);
